002b868dab2434e23a90f26d844c37d4fced6dcb
DominicR <dojuparupe1975@gmail.com>
2024-12-08 00:40:18 -0600

    prueba

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import  {BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Home />
     </BrowserRouter>
   </StrictMode>,
 )
